df6c8650bcb1a2639fab0ae68f5786879c2bbf21
cardoso-thiago <thiago.cardoso33@gmail.com>
2021-06-05 18:40:37 -0300

    Melhoria no código para conversão para Kotlin

--- kotlin-java/src/main/java/br/com/cardoso/ReadMessages.java
+++ kotlin-java/src/main/java/br/com/cardoso/ReadMessages.java
@@ -3,14 +3,21 @@ package br.com.cardoso;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadMessages {
 
     public List<String> readContentFromFile() throws URISyntaxException, IOException {
-        File file = new File(ReadMessages.class.getClassLoader().getResource("messages.txt").toURI());
-        return Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        URL resource = ReadMessages.class.getClassLoader().getResource("messages.txt");
+        if (resource != null) {
+
+            File file = new File(resource.toURI());
+            return Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        }
+        return new ArrayList<>();
     }
 }
